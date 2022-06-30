c79064b89325887cc7f1df0129a4580219ada80c
antoniu.pitic <antoniu.pitic@gmail.com>
2022-06-30 11:46:22 +0300

    Add the line to accept the calls @CrossOrigin(origins = "*", allowedHeaders = "*")

--- src/main/java/com/example/helloworld/Controllers/Salutations.java
+++ src/main/java/com/example/helloworld/Controllers/Salutations.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/Salutations")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class Salutations {
     @GetMapping("/Hi")
     public String sayHi(@RequestParam(value = "name") String name) {
